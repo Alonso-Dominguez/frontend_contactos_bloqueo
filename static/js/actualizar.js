a2f28c3944e4be0265eb3d6d406e9a32a5b4796e
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-13 00:37:29 -0600

    Update actualizar.js

--- static/js/actualizar.js
+++ static/js/actualizar.js
@@ -1,5 +1,5 @@
 async function actualizarContacto() {
-    const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/actualizar_contactos/';
+    const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/actualizar_contactos/{email}';
 
     // Obtén el token desde donde lo tengas almacenado
     const token = localStorage.getItem('token');
