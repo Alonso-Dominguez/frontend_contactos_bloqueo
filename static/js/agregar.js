d175ec964fdc2ca01e98af70acb61a8cbc9246f9
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-13 11:14:06 -0600

    Update agregar.js

--- static/js/agregar.js
+++ static/js/agregar.js
@@ -9,7 +9,7 @@ async function agregarContacto() {
     const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/contactos';
 
     // Obtén el token desde donde lo tengas almacenado
-    const token = localStorage.getItem('token');
+    const token = sessionStorage.getItem('token');
 
     try {
         const nombre = document.getElementById('nombre').value;
