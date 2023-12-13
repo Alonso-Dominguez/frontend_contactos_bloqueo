36a14188add15207785746055f70908e9969b93e
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-13 00:30:14 -0600

    Update actualizar.js

--- static/js/actualizar.js
+++ static/js/actualizar.js
@@ -1,46 +1,44 @@
 async function actualizarContacto() {
-    const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/contactos';
+    const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/actualizar_contactos/';
+
     // Obtén el token desde donde lo tengas almacenado
     const token = localStorage.getItem('token');
 
     try {
-        const email = document.getElementById('actualizar-id').value;
+        const email = document.getElementById('actualizar-email').value;
         const nuevoNombre = document.getElementById('actualizar-nombre').value;
         const nuevoApellido_paterno = document.getElementById('actualizar-apellido-paterno').value;
         const nuevoApellido_materno = document.getElementById('actualizar-apellido-materno').value;
-        const nuevoEmail = document.getElementById('actualizar-email').value;
         const nuevoTelefono = document.getElementById('actualizar-telefono').value;
 
-        const headers = {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json',
-        };
+        const headers = new Headers({
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json",
+        });
 
-        const data = {
+        const body = JSON.stringify({
             nombre: nuevoNombre,
             primer_apellido: nuevoApellido_paterno,
             segundo_apellido: nuevoApellido_materno,
-            email: nuevoEmail,
             telefono: nuevoTelefono,
-        };
+        });
 
-        // Actualiza la URL para incluir el ID en lugar del email
-        const response = await axios.put(`${apiURL}/${email}`, data, { headers });
+        const response = await fetch(`${apiURL}/${email}`, {
+            method: 'PUT',
+            headers: headers,
+            body: body,
+        });
 
-        if (response.status === 200) {
+        if (response.ok) {
             alert('Contacto actualizado correctamente.');
-            document.location.reload();
+            // Redirige a la página de inicio después de la actualización
+            window.location.href = "/inicio";
         } else {
-            alert(`Error al actualizar el contacto: ${response.data.detail}`);
+            const errorData = await response.json();
+            alert(`Error al actualizar el contacto: ${errorData.detail}`);
         }
     } catch (error) {
         console.error('Error inesperado:', error);
         alert('Error inesperado al actualizar el contacto.');
     }
 }
-
-// Actualizar el formulario cuando se presiona "Enter"
-document.getElementById('updateContactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    actualizarContacto();
-});
