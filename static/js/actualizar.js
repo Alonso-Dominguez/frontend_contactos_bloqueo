8e4e2fb4d2d9f67b6538a3c85c2c715211599af5
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-12 23:49:39 -0600

    Update actualizar.js

--- static/js/actualizar.js
+++ static/js/actualizar.js
@@ -5,38 +5,34 @@ async function actualizarContacto() {
     const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/contactos';
 
     try {
-        const email = document.getElementById('actualizar-id').value;
+        const id = document.getElementById('actualizar-id').value;
         const nuevoNombre = document.getElementById('actualizar-nombre').value;
         const nuevoApellido_paterno = document.getElementById('actualizar-apellido-paterno').value;
         const nuevoApellido_materno = document.getElementById('actualizar-apellido-materno').value;
         const nuevoEmail = document.getElementById('actualizar-email').value;
         const nuevoTelefono = document.getElementById('actualizar-telefono').value;
 
-        const headers = new Headers({
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json",
-        });
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        };
 
-        const body = JSON.stringify({
+        const data = {
             nombre: nuevoNombre,
             primer_apellido: nuevoApellido_paterno,
             segundo_apellido: nuevoApellido_materno,
             email: nuevoEmail,
             telefono: nuevoTelefono,
-        });
+        };
 
-        const response = await fetch(`${apiURL}/${email}`, {
-            method: 'PUT',
-            headers: headers,
-            body: body,
-        });
+        // Actualiza la URL para incluir el ID en lugar del email
+        const response = await axios.put(`${apiURL}/${id}`, data, { headers });
 
-        if (response.ok) {
+        if (response.status === 200) {
             alert('Contacto actualizado correctamente.');
             document.location.reload();
         } else {
-            const errorData = await response.json();
-            alert(`Error al actualizar el contacto: ${errorData.detail}`);
+            alert(`Error al actualizar el contacto: ${response.data.detail}`);
         }
     } catch (error) {
         console.error('Error inesperado:', error);
@@ -44,4 +40,8 @@ async function actualizarContacto() {
     }
 }
 
-actualizarContacto();
+// Actualizar el formulario cuando se presiona "Enter"
+document.getElementById('updateContactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    actualizarContacto();
+});
