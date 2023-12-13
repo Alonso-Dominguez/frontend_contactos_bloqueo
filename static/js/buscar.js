f91daa1f9ffbaebc3e0bb6ae3685be17b7b4c2b4
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-13 00:46:31 -0600

    Update buscar.js

--- static/js/buscar.js
+++ static/js/buscar.js
@@ -1,5 +1,5 @@
 async function buscarContactos() {
-    const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/contactos/buscar';
+    const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/contactos';
 
     // Obtén el token desde donde lo tengas almacenado
     const token = localStorage.getItem('token');
