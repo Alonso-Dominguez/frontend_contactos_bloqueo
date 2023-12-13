c5c1ac36b42ecd5a41ef722cd6e45a15c0bba080
Alonso-Dominguez <122809477+Alonso-Dominguez@users.noreply.github.com>
2023-12-13 00:51:35 -0600

    Update borrar.js

--- static/js/borrar.js
+++ static/js/borrar.js
@@ -1,33 +1,30 @@
-async function borrarContacto() {
+// Obtén el token desde donde lo tengas almacenado
+const token = localStorage.getItem('token');
+
+async function eliminarContacto() {
     const apiURL = 'https://backend-contactos-bloqueo-fb3d5fd89684.herokuapp.com/contactos';
 
-    // Obtén el token desde donde lo tengas almacenado
-    const token = localStorage.getItem('token');
-
     try {
-        const id = document.getElementById('borrar-id').value;
+        const email = document.getElementById('eliminar-email').value;
 
         const headers = new Headers({
             "Authorization": `Bearer ${token}`,
             "Content-Type": "application/json",
         });
 
-        const response = await fetch(`${apiURL}/${id}`, {
+        const response = await fetch(`${apiURL}/${email}`, {
             method: 'DELETE',
             headers: headers,
         });
 
         if (response.ok) {
-            alert('Contacto borrado correctamente.');
-            document.location.reload();
+            alert('Contacto eliminado correctamente.');
         } else {
             const errorData = await response.json();
-            alert(`Error al borrar el contacto: ${errorData.detail}`);
+            alert(`Error al eliminar el contacto: ${errorData.detail}`);
         }
     } catch (error) {
         console.error('Error inesperado:', error);
-        alert('Error inesperado al borrar el contacto.');
+        alert('Error inesperado al eliminar el contacto.');
     }
 }
-
-borrarContacto();
